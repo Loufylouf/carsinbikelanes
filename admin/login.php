09dd94454c3fa393ef75573036fc935b06dea4ad
Laurent Louf <laurentloug@gmail.com>
2016-10-08 10:55:59 +0200

    Indentation for admin login.

--- admin/login.php
+++ admin/login.php
@@ -1,31 +1,29 @@
 <html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="../css/style.css" />
+      <link href='//fonts.googleapis.com/css?family=Oswald:400,700|Francois+One' rel='stylesheet' type='text/css'>
+   </head>
 
-<!-- main stylesheet -->
-<link rel="stylesheet" type="text/css" href="../css/style.css" />
+   <body class="non_map">
 
-<!-- google fonts -->
-<link href='//fonts.googleapis.com/css?family=Oswald:400,700|Francois+One' rel='stylesheet' type='text/css'>
+      <div class="flex_container_dialog">
+         <div class="setup_centered">
 
-<body class="non_map">
+            <div class="settings_group">
+               <form action="index.php" method="post">
+                  <h3>Administration</h3>
+                  <span>username: </span><input id="username" class="wide" type="text" name="username">
+                  <span>password: </span><input id="password" class="wide" type="password" name="password">
+                  <input type="submit" class="wide" value="LOGIN">
+               </form>
+            </div>
 
-<div class="flex_container_dialog">
-<div class="setup_centered">
+         </div>
+      </div>
 
-<div class="settings_group">
-<form action="index.php" method="post">
-<h3>Administration</h3>
-<span>username: </span><input id="username" class="wide" type="text" name="username">
-<span>password: </span><input id="password" class="wide" type="password" name="password">
-<input type="submit" class="wide" value="LOGIN">
-</form>
-</div>
+   </body>
 
-</div>
-</div>
-
-</body>
-
-<script type="text/javascript">
-document.getElementById("username").focus();
-</script>
+   <script type="text/javascript">
+      document.getElementById("username").focus();
+   </script>
 </html>
